83f0b4abc029055dec7aeb8a582fcf3debaa18fd
Maicol <maicolbattistini@live.it>
2020-09-20 14:15:47 +0200

    chore: Updated example

--- example/index.php
+++ example/index.php
@@ -1,9 +1,12 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Maicol07\SSO\Flarum;
 
-require_once __DIR__ . '/src/Flarum.php';
+// Note: Since this is called from the example folder, the vendor folder is located in the previous tree level
+require_once __DIR__ . '/../vendor/autoload.php';
 
+// Dummy users
 $users = [
     'user' => [
         'password' => 'password',
@@ -15,18 +18,27 @@ $users = [
     ],
 ];
 
-$username = empty($_POST['username']) ? '' : $_POST['username'];
-$password = empty($_POST['password']) ? '' : $_POST['password'];
+// Get username and password
+$username = $_POST['username'] ?? '';
+$password = $_POST['password'] ?? '';
 
 if (isset($users[$username]) && $users[$username]['password'] === $password) {
     $email = $users[$username]['email'];
+    
     // Create the Flarum object with the required configuration. The parameters are explained in the class file (src/Flarum.php)
-    $forum = new Flarum('http://flarum.example.com', 'example.com', 'NotSecureToken', 'NotSecureToken');
+    $forum = new Flarum(
+        env('FLARUM_HOST') ?? 'http://flarum.example.com',
+        env('ROOT_DOMAIN') ?? 'example.com',
+        env('API_KEY') ?? 'NotSecureToken',
+        env('PASSWORD_TOKEN') ?? 'NotSecureToken'
+    );
+    
     // Login the user with username, email and password (if user is already signed up in Flarum before using this extension)
     // If user doesn't exists in Flarum, it will be created
     $forum->login($username, $email, $users[$username]['password']);
+    
     // Redirect to Flarum
-    $forum->redirectToForum();
+    $forum->redirect();
 } elseif (!empty($username) || !empty($password)) {
     echo 'Login failed';
 }
@@ -34,11 +46,18 @@ if (isset($users[$username]) && $users[$username]['password'] === $password) {
 
 <h1>Login</h1>
 
-<p><?= array_keys($users)[0] ?> / <?= $users['user']['password'] ?></p>
-<p><?= array_keys($users)[1] ?> / <?= $users['admin']['password'] ?></p>
+<?php
+foreach ($users as $user => $details) {
+    echo '<p>' . $user . ' / ' . Arr::get($details, 'password') . '</p>';
+}
+?>
 
 <form method="post">
-    <input type="text" name="username" placeholder="Username">
-    <input type="password" name="password" placeholder="Password">
+    <label>
+        <input type="text" name="username" placeholder="Username">
+    </label>
+    <label>
+        <input type="password" name="password" placeholder="Password">
+    </label>
     <button type="submit">Login</button>
 </form>
--- example/logout.php
+++ example/logout.php
@@ -1,12 +1,21 @@
 <?php
 
-require_once __DIR__ . '/src/Flarum.php';
+// Note: Since this is called from the example folder, the vendor folder is located in the previous tree level
+use Maicol07\SSO\Flarum;
 
-$forum = new Flarum('http://flarum.example.com', 'example.com', 'NotSecureToken', 'NotSecureToken');
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Create the Flarum object with the required configuration. The parameters are explained in the class file (src/Flarum.php)
+$forum = new Flarum(
+    env('FLARUM_HOST') ?? 'http://flarum.example.com',
+    env('ROOT_DOMAIN') ?? 'example.com',
+    env('API_KEY') ?? 'NotSecureToken',
+    env('PASSWORD_TOKEN') ?? 'NotSecureToken'
+);
 
 // Logout current user
 $forum->logout();
 
-if ($_GET['forum']) {
-    $forum->redirectToForum();
+if (!empty($_GET['redirect'])) {
+    $forum->redirect();
 }
