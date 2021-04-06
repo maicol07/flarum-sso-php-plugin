e886c596b6e6bdc9c8cf3543bffb732617c8e118
Maicol <maicolbattistini@live.it>
2021-04-06 16:33:46 +0200

    update(example): Updated example

--- example/index.php
+++ example/index.php
@@ -147,26 +147,7 @@ if (!empty(Arr::get($users, $username)) && Arr::get($users, "$username.password"
         </div>
     <?php } ?>
 </div>
-<footer class="footer">
-    <div class="content has-text-centered">
-        <div class="field is-grouped" style="justify-content: center;">
-            <p class="control">
-                <a class="button is-link" href="index.php">
-                    Login
-                </a>
-            </p>
-            <p class="control">
-                <a class="button" href="logout.php">
-                    Logout
-                </a>
-            </p>
-            <p class="control">
-                <a class="button is-danger" href="delete.php?username=user">
-                    Delete user
-                </a>
-            </p>
-        </div>
-    </div>
-</footer>
+
+<?php require_once 'footer.php' ?>
 </body>
 </html>
